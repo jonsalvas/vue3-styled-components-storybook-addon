4bd4c00121af4af3c0917b5cfa7b1b3d719d7185
jonsalvas <>
2021-12-24 15:19:59 +0100

    some bugfixing

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ if (module && module.hot && module.hot.decline) {
   module.hot.decline();
 }
 
+export {withThemeProvider} from "./withThemeProvider";
 // make it work with --isolatedModules
 export default {};
